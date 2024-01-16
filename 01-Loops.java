f9dc8c9fcb2910763eb0810a1bd751cdc7bf89e4
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-16 19:32:26 +0530

    Updated Commit

--- 01-Loops.java
+++ 01-Loops.java
@@ -1,19 +1,49 @@
 // Working of Loops
 
+// import java.util.Scanner;
+// class myClass{
+//     public static void main(String[] args){
+//         Scanner s = new Scanner(System.in);
+//         System.out.println("Enter a number: ");
+//         int age = s.nextInt();
+//         if(age>18){
+//             System.out.println("You can vote now!");
+//         }
+//         else if(age==18){
+//             System.out.println("You can't vote now!");
+//         }else{
+//             System.out.println("Wait now!");
+//         }
+//         s.close();   
+//     }
+// }
+
+//QuesMale:
 import java.util.Scanner;
-class myClass{
-    public static void main(String[] args){
+
+class myClass {
+    public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.println("Enter a number: ");
-        int age = s.nextInt();
-        if(age>18){
-            System.out.println("You can vote now!");
-        }
-        else if(age==18){
-            System.out.println("You can't vote now!");
-        }else{
-            System.out.println("Wait now!");
-        }
-        s.close();   
+        System.out.println("Enter a gender: ");
+        String gender = s.nextLine();
+        
+        if (gender.equalsIgnoreCase("Male")) {
+            System.out.println("Enter an age: ");
+            int age = s.nextInt();
+            if (age > 23) {
+                System.out.println("You can vote!");
+            } else {
+                System.out.println("Wait now!");
+            }
+        } else {
+            System.out.println("Enter an age: ");
+            int age = s.nextInt();
+            if (age > 18) {
+                System.out.println("You can vote!");
+            } else {
+                System.out.println("Wait now!");
+            }
+        }    
+        s.close();
     }
 }
