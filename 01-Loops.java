7c22179f7e0b93a308c969eff8c175f3912d4075
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-16 19:33:28 +0530

    Updated Commit

--- 01-Loops.java
+++ 01-Loops.java
@@ -18,32 +18,33 @@
 //     }
 // }
 
-//QuesMale:
-import java.util.Scanner;
+//Ques:
 
-class myClass {
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        System.out.println("Enter a gender: ");
-        String gender = s.nextLine();
+// import java.util.Scanner;
+
+// class myClass {
+//     public static void main(String[] args) {
+//         Scanner s = new Scanner(System.in);
+//         System.out.println("Enter a gender: ");
+//         String gender = s.nextLine();
         
-        if (gender.equalsIgnoreCase("Male")) {
-            System.out.println("Enter an age: ");
-            int age = s.nextInt();
-            if (age > 23) {
-                System.out.println("You can vote!");
-            } else {
-                System.out.println("Wait now!");
-            }
-        } else {
-            System.out.println("Enter an age: ");
-            int age = s.nextInt();
-            if (age > 18) {
-                System.out.println("You can vote!");
-            } else {
-                System.out.println("Wait now!");
-            }
-        }    
-        s.close();
-    }
-}
+//         if (gender.equalsIgnoreCase("Male")) {
+//             System.out.println("Enter an age: ");
+//             int age = s.nextInt();
+//             if (age > 23) {
+//                 System.out.println("You can vote!");
+//             } else {
+//                 System.out.println("Wait now!");
+//             }
+//         } else {
+//             System.out.println("Enter an age: ");
+//             int age = s.nextInt();
+//             if (age > 18) {
+//                 System.out.println("You can vote!");
+//             } else {
+//                 System.out.println("Wait now!");
+//             }
+//         }    
+//         s.close();
+//     }
+// }
