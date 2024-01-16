101888dad2c09e470832c76d3806c51478039eda
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-16 19:19:46 +0530

    Updated Commit

--- 01-Loops.java
+++ 01-Loops.java
@@ -1,2 +1,18 @@
 // Working of Loops
 
+import java.util.Scanner;
+class myClass{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int age = s.nextInt();
+        if(age>18){
+            System.out.println("You can vote now!");
+        }
+        else if(age==18){
+            System.out.println("You can't vote now!");
+        }else{
+            System.out.println("You can't vote now!");
+        }   
+    }
+}
