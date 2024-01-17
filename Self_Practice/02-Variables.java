3613755d695e4b38cf4e696626b83450eb206631
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-17 20:19:08 +0530

    Updated Commit

---
+++ Self_Practice/02-Variables.java
@@ -0,0 +1,46 @@
+package Self_Practice;
+
+//defining variables:
+
+// import java.util.Scanner;
+// class hey{
+// public static void main(String[] args){
+//     Scanner s = new Scanner(System.in);
+//     String name = "Arpita";
+//     System.out.println(name);
+// }
+// };
+
+
+// import java.util.Scanner;
+// class hey{
+//     public static void main(String[] args){
+//         int Num = 15;
+//         System.out.println(Num);
+//     }
+// };
+
+
+// import java.util.Scanner;
+// class hey{
+//     public static void main(String[] args){
+//         int Num = 23;
+//         String Name = "Arpita";
+//         float num2 = 5.99f;
+//         char letter1 = 'H';
+//         System.out.println("Serial no: " + Num);
+//         System.out.println("Name: " +Name );
+//         System.out.println("Num 2 denotes: "+num2);
+//         System.out.println("Letter she loves: "+letter1);
+//     }
+// };
+
+import java.util.Scanner;
+class hey{
+    public static void main(String[] args){
+        int x = 10;
+        int y = 20;
+        int z = 5;
+        System.out.println(x+y+z);
+    }
+};
