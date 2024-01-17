4d1afa9057b2563ebe10776862e01880dc21926b
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-17 20:27:17 +0530

    Updated Commit

--- Self_Practice/03-Identifiers.java
+++ Self_Practice/03-Identifiers.java
@@ -1,3 +1,14 @@
 package Self_Practice;
 
 
+//identifiers: All Java variables must be identified with unique names.
+//These unique names are called identifiers.
+
+import java.util.Scanner;
+class hello{
+    public static void main(String[] args){
+        int minutesPerHour = 60; //good practice
+        int m = 60;   //bad practice
+        System.out.println(minutesPerHour);
+    }
+};
