5ef64e189832453f79850720ea0d74ab3fdbf477
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-17 19:44:32 +0530

    Updated Commit

--- 01-Loops.java
+++ 01-Loops.java
@@ -134,6 +134,6 @@ import java.util.Scanner;
 class myClass{
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
-        System.out.println("")
+        System.out.println("");
     }
 }
---
+++ Self_Practice/01-Basics.java
@@ -0,0 +1,16 @@
+package Self_Practice;
+
+
+//Hello world printing
+// import java.util.Scanner;
+
+// class Hello{
+//     public static void main(String[] args){
+//         Scanner s = new Scanner(System.in);
+//         System.out.println("Hello World!!");
+//     }
+// }
+
+//java is owned by oracle;
+//it is open source abd free to use;it is OOPs based programming language.
+//
