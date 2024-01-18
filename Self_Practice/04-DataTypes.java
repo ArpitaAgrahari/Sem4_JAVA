1572fed2c75938ec411a023dc527f71054c24a87
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-18 15:38:20 +0530

    Updated Commit

--- Self_Practice/04-DataTypes.java
+++ Self_Practice/04-DataTypes.java
@@ -1,5 +1,21 @@
-package Self_Practice;
+// package Self_Practice;
 
-public class 04-DataTypes {
-    
-}
+// import java.util.Scanner;
+// class hey{
+//     public static void main(String[] args){
+//     int num = 5;
+//     float floatnum = 10.909f;
+//     char letter = 20;
+//     boolean mybool = true;
+//     String text = "hello";
+//     String text2 = "Hello World !!";
+//     String text3 = "hey";
+//     System.out.println(num.getClass()+" is data type of num.");
+//     System.out.println(floatnum.getClass() +" is data type of floatnum.");
+//     System.out.println(letter.getClass()+" is data type of letter.");
+//     System.out.println(mybool.getClass()+" is data type of mybool.");
+//     System.out.println(text.getClass()+" is data type of text.");
+//     System.out.println(text2.getClass()+" is data type of text2.");
+//     System.out.println(text3.getClass()+" is data type of text3.");
+//     }
+// };
---
+++ Self_Practice/05-Type_Comversion.java
@@ -0,0 +1,11 @@
+package Self_Practice;
+//A String in Java is actually a non-primitive data type, because it refers to an object. The String object has methods that are used to perform certain operations on strings.
+import java.util.Scanner;
+public static void  main(String[] args){
+class hello{
+
+}
+};
+int main(){
+
+}
