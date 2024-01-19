1a1d2c6838e8c2c33eae691cdbee32539cc52ad8
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-19 13:38:00 +0530

    Updated Commit

--- Self_Practice/05-Type_Conversion.java
+++ Self_Practice/05-Type_Conversion.java
@@ -1,15 +1,35 @@
 package Self_Practice;
 
-import java.util.Scanner;
 
-class hello{
-    public static void main(String[] args){
-        int num = 9;
-        double num2 = num;
-        float num3 = num;
-        System.out.println(num);
-        System.out.println(num2);
-        System.out.println(num3);
-    } // Remove the unnecessary semicolon here
-} // Add a closing brace here
+//Widening Casting
+
+
+// import java.util.Scanner;
+
+// class hello{
+//     public static void main(String[] args){
+//         int num = 9;
+//         double num2 = num;
+//         float num3 = num;
+//         System.out.println(num);
+//         System.out.println(num2);
+//         System.out.println(num3);
+//     } // Remove the unnecessary semicolon here
+// } // Add a closing brace here
+
+
+
+//Narrowing Casting
+
+// import java.util.Scanner;
+
+// class hey{
+//     public static void main(String[] args){
+//         double mydouble = 9.74d;
+//         int myint = (int) mydouble;
+//         System.out.println(mydouble);
+//         System.out.println(myint);
+//     }
+// }
+
 
