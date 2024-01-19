c61841efd51e3fd11f87a840749521f5ea8b40e8
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-19 14:04:51 +0530

    Updated Commit

--- Self_Practice/06-String.java
+++ Self_Practice/06-String.java
@@ -2,18 +2,32 @@ package Self_Practice;
 
 
 //Strings are used for storing text.
+// import java.util.Scanner;
+// class stringInJava{       
+//     public static void main(String[] args){
+//         String greeting = "Hello!! Harshit";
+//         //A String in Java is actually an object, which contain methods that can perform certain operations on strings.
+//         System.out.println("the length of greeting is:" + greeting.length());
+//         //upper case printing or converasion
+//         System.out.println("The upper case of greting is: " +greeting.toUpperCase());
+//         //lower acse conversion
+//         System.out.println("the lower Case of greeting is: "+greeting.toLowerCase());
+//         //Java counts positions from zero.0 is the first position in a string, 1 is the second, 2 is the third ...
+//         System.out.println("The index of given letter is:" + greeting.indexOf("Harshit"));
+
+//     }
+// }
+
+
+//STRING CONCATENATION
+
+//The + operator can be used between strings to combine them. This is called concatenation.
+
 import java.util.Scanner;
-class stringInJava{       
+class hi{
     public static void main(String[] args){
-        String greeting = "Hello!! Harshit";
-        //A String in Java is actually an object, which contain methods that can perform certain operations on strings.
-        System.out.println("the length of greeting is:" + greeting.length());
-        //upper case printing or converasion
-        System.out.println("The upper case of greting is: " +greeting.toUpperCase());
-        //lower acse conversion
-        System.out.println("the lower Case of greeting is: "+greeting.toLowerCase());
-        //Java counts positions from zero.0 is the first position in a string, 1 is the second, 2 is the third ...
-        System.out.println("The index of given letter is:" + greeting.indexOf("Harshit"));
-        
+        String firstName = "Arpita";
+        String lastname = "Agrahari";
+        System.out.println("Your full name is :" + firstName +" "+lastname);
     }
 }
