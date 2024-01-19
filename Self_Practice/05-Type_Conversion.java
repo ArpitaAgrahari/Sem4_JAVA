2e06b1600d4d3d93ce006d1af98760748be6059b
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-19 13:34:38 +0530

    Updated Commit

--- Self_Practice/05-Type_Conversion.java
+++ Self_Practice/05-Type_Conversion.java
@@ -6,8 +6,10 @@ class hello{
     public static void main(String[] args){
         int num = 9;
         double num2 = num;
-    }
-}
-int main(){
-    
-}
+        float num3 = num;
+        System.out.println(num);
+        System.out.println(num2);
+        System.out.println(num3);
+    } // Remove the unnecessary semicolon here
+} // Add a closing brace here
+
