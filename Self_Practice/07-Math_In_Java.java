7bb177ec48ecfe82ddb2c4bb1997d53c27552dc7
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-19 14:36:54 +0530

    Updated Commit

--- Self_Practice/07-Math_In_Java.java
+++ Self_Practice/07-Math_In_Java.java
@@ -1,3 +1,26 @@
-package Self_Practice;
+import java.util.Scanner;
+import java.lang.Math;
+// No package statement needed for this code
+
+class hi{
+    public static void main(String[] args){
+
+        System.out.println("the max number is:");
+        //The Math.max(x,y) method can be used to find the highest value of x and y:
+        Math.max(5,10);
+        System.out.println("the min number is:");
+        //The Math.min(x,y) method can be used to find the lowest value of x and y:
+        Math.min(5,10);
+        System.out.println("the square of given number is:");
+        //Math.sqrt(x): The Math.sqrt(x) method returns the square root of x
+        Math.sqrt(5);
+        System.out.println("the absolute value of number is:");
+        //Math.abs(x): The Math.abs(x) method returns the absolute (positive) value of x
+        Math.abs(-4.7);
+        System.out.println("the random number is:");
+        //Random Numbers: Math.random() returns a random number between 0.0 (inclusive), and 1.0 (exclusive)
+        Math.random();
 
 
+    }
+}
