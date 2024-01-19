3444b55efbac6c8e69e76ce25007489b5907468a
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-19 13:45:39 +0530

    Updated Commit

--- Self_Practice/05-Type_Conversion.java
+++ Self_Practice/05-Type_Conversion.java
@@ -33,3 +33,39 @@ package Self_Practice;
 // }
 
 
+
+
+
+
+//OPERATOR IN JAVA
+
+//Java divides the operators :
+
+// Arithmetic operators
+// Assignment operators
+// Comparison operators
+// Logical operators
+// Bitwise operators
+
+// import java.util.Scanner;
+// class hi{
+//     public static void main(String[] args){
+//     int x = 100+50;
+//     int y = 100-50;
+//     int z = 100*50;
+//     int a = 100%50;
+//     int b = a++;
+//     int c = a--;
+//     int d = --a;
+//     int e = ++a;
+//     System.out.println(x);
+//     System.out.println(y);
+//     System.out.println(z);
+//     System.out.println(a);
+//     System.out.println(b);
+//     System.out.println(c);
+//     System.out.println(d);
+//     System.out.println(e);
+//     }
+// }
+
