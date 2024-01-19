bdf65bd4b960294825b2ca3834abe0a6dd075ae5
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-19 13:29:52 +0530

    Updated Commit

---
+++ Self_Practice/05-Type_Conversion.java
@@ -0,0 +1,13 @@
+package Self_Practice;
+
+import java.util.Scanner;
+
+class hello{
+    public static void main(String[] args){
+        int num = 9;
+        double num2 = num;
+    }
+}
+int main(){
+    
+}
