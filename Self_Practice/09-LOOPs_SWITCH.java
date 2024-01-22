0d2688edf8314176bfc9d8ee710eb1f7d30d7170
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-22 12:11:15 +0530

    Updated Commit

--- Self_Practice/09-LOOPs_SWITCH.java
+++ Self_Practice/09-LOOPs_SWITCH.java
@@ -63,13 +63,25 @@ package Self_Practice;
 // }
 
 
-import java.util.Scanner;
-class table{
-    public static void main(String[] args){
-        for(int i=0;i<20;i++){
-            if(i%2 == 0){
-                System.out.println(i);
-            }
-        }
-    }
-}
+// import java.util.Scanner;
+// class table{
+//     public static void main(String[] args){
+//         for(int i=0;i<20;i++){
+//             if(i%2 == 0){
+//                 System.out.println(i);
+//             }
+//         }
+//     }
+// }
+
+
+// import java.util.Scanner;
+// class tableOf3{
+//     public static void main(String[] args){
+//         for(int i=0;i<15;i++){
+//             if(i%2 != 0){
+//                 System.out.println(i);
+//             }
+//         }
+//     }
+// }
