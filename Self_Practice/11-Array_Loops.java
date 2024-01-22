1f336e37f94eeb65829ad5b7fc8d8bb1b931a04d
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-22 12:54:02 +0530

    Updated Commit

--- Self_Practice/11-Array_Loops.java
+++ Self_Practice/11-Array_Loops.java
@@ -47,10 +47,24 @@ You can loop through the array elements with the for loop, and use the length pr
 
 
 //MULTIDIMENSIONAL ARRAY
-import java.util.Scanner;
-class matrixCreation{
-    public static void main(String[] args){
-        int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
-            System.out.println(matrix[1][1]);
-    }
-}
+
+// import java.util.Scanner;
+// class matrixCreation{
+//     public static void main(String[] args){
+//         int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
+//             System.out.println(matrix[1][1]);
+//     }
+// }
+
+
+// import java.util.Scanner;
+// class matrixPrinting{
+//     public static void main(String[] args){
+//         int[][] nums = {{1,2,3},{4,5,6},{7,8,9}};
+//         for(int i = 0;i<nums.length;++i){
+//             for(int j=0;j<nums[i].length;++j){
+//                 System.out.println(nums[i][j]);
+//             }
+//         }
+//     }
+// }
