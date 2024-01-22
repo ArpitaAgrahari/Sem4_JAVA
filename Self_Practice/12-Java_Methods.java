c174605b1b29bce1f7ef2f810ac8e1dd9c46dbb9
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-22 12:57:16 +0530

    Updated Commit

--- Self_Practice/12-Java_Methods.java
+++ Self_Practice/12-Java_Methods.java
@@ -1,3 +1,16 @@
 package Self_Practice;
 
 
+/*
+ A method is a block of code which only runs when it is called.
+
+You can pass data, known as parameters, into a method.
+
+Methods are used to perform certain actions, and they are also known as functions.
+ */
+import java.util.Scanner;
+class methodLearn{
+    public static void main(String[] args){
+        
+    }
+}
