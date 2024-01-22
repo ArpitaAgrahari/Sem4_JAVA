bb8da8d384007e152051195681d5c01b7d8ce71f
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-22 12:08:28 +0530

    Updated Commit

--- Self_Practice/09-LOOPs_SWITCH.java
+++ Self_Practice/09-LOOPs_SWITCH.java
@@ -61,3 +61,15 @@ package Self_Practice;
 //         }
 //     }
 // }
+
+
+import java.util.Scanner;
+class table{
+    public static void main(String[] args){
+        for(int i=0;i<20;i++){
+            if(i%2 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
