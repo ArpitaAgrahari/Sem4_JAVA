8a3cccff49baf60d5e322dac65f3f59bfee8a1c9
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-22 12:38:13 +0530

    Updated Commit

--- Self_Practice/11-Array_Loops.java
+++ Self_Practice/11-Array_Loops.java
@@ -6,7 +6,6 @@ class loopingArray{
         String[] name = {"John","Shyam","Siya","ram"};
         for(int i = 0;i<name.length;i++){
             System.out.println(name[i]);
-            
         }
     }
 }
