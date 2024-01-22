0c08922baba426cc0a2e1da7226de4c796efc4df
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-22 12:37:18 +0530

    Updated Commit

--- Self_Practice/11-Array_Loops.java
+++ Self_Practice/11-Array_Loops.java
@@ -1,8 +1,11 @@
 package Self_Practice;
 
-import java.util.Scanenr;
+import java.util.Scanner;
 class loopingArray{
     public static void main(String[] args){
-
+        String[] name = {"John","Shyam","Siya","ram"};
+        for(int i = 0;i<name.length;i++){
+            System.ot.println(name[i]);
+        }
     }
 }
