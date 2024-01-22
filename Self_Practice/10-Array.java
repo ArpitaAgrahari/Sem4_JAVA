d1e17d3ed241fddfc61705abb732711602a22523
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-22 12:32:13 +0530

    Updated Commit

--- Self_Practice/10-Array.java
+++ Self_Practice/10-Array.java
@@ -1,13 +1,26 @@
 package Self_Practice;
 
-import java.util.Scanner;
-class arrayLearn{
-    public static void main(String[] args){
-        int[] nums = {10,20,30,40,50};
-        System.out.println(nums[0]);
-        System.out.println(nums[1]);
-        System.out.println(nums[2]);
-        System.out.println(nums[3]);
-        System.out.println(nums[4]);
-    }
-}
+// import java.util.Scanner;
+// class arrayLearn{
+//     public static void main(String[] args){
+//         int[] nums = {10,20,30,40,50};
+//         System.out.println(nums[0]);
+//         System.out.println(nums[1]);
+//         System.out.println(nums[2]);
+//         System.out.println(nums[3]);
+//         System.out.println(nums[4]);
+//     }
+// }
+
+
+//modification of array element
+
+
+// import java.util.Scanner;
+// class arrayElem{
+//     public static void main(String[] args){
+//         String[] name= {"Arpita","Harhit","Iram","Akshu"};
+//         name[3] = "Akshara";
+//         System.out.println(name[3]);
+//     }
+// }
