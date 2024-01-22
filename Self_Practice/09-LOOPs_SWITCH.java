6f1707b7d36ecc29b3ee404d40c33afaf3e69cbc
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-22 11:57:17 +0530

    Updated Commit

--- Self_Practice/09-LOOPs_SWITCH.java
+++ Self_Practice/09-LOOPs_SWITCH.java
@@ -3,6 +3,19 @@ package Self_Practice;
 import java.util.Scanner;
 class hey{
     public static void main(String[] args){
-        System.out.println("Hello!");
+        int x = 2;
+        switch(2){
+            case 1:
+               System.out.println("Hey1 I am 1");
+               break;
+            case 2:
+                System.out.println("Hey! I am 2");
+                break;
+            case 3:
+                System.out.println("Hey! I am 3");
+                break;
+            default:
+                System.out.println("Hey!. I am default");
+        }
     }
 }
