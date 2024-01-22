08170e77af3819a659faffab4f23ab1cc568802b
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-22 12:28:46 +0530

    Updated Commit

--- Self_Practice/10-Array.java
+++ Self_Practice/10-Array.java
@@ -1,5 +1,13 @@
 package Self_Practice;
 
-public class 10-Array {
-    
+import java.util.Scanner;
+class arrayLearn{
+    public static void main(String[] args){
+        int[] nums = {10,20,30,40,50};
+        System.out.println(nums[0]);
+        System.out.println(nums[1]);
+        System.out.println(nums[2]);
+        System.out.println(nums[3]);
+        System.out.println(nums[4]);
+    }
 }
