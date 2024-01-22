aa3bd7493d3c96508f48ba5c2b9613f1e9f384db
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-22 12:44:16 +0530

    Updated Commit

--- Self_Practice/11-Array_Loops.java
+++ Self_Practice/11-Array_Loops.java
@@ -1,11 +1,46 @@
 package Self_Practice;
 
-import java.util.Scanner;
-class loopingArray{
-    public static void main(String[] args){
-        String[] name = {"John","Shyam","Siya","ram"};
-        for(int i = 0;i<name.length;i++){
-            System.out.println(name[i]);
-        }
-    }
-}
+
+
+/*
+ Loop Through an Array:
+You can loop through the array elements with the for loop, and use the length property to specify how many times the loop should run.
+
+*/
+
+// import java.util.Scanner;
+// class loopingArray{
+//     public static void main(String[] args){
+//         String[] name = {"John","Shyam","Siya","ram"};
+//         for(int i = 0;i<name.length;i++){
+//             System.out.println(name[i]);
+//         }
+//     }
+// }
+
+
+/*FOR_EACH LOOP: 
+  "for-each" loop, which is used exclusively to loop through elements in arrays
+*/
+
+// import java.util.Scanner;
+// class print{
+//     public static void main(String[] args){
+//         String[] Car= {"Volvo","BMW","Ford","Safari"};
+//         for(String i:Car){
+//             System.out.println(i);
+//         }
+//     }
+// }
+
+
+
+// import java.util.Scanner;
+// class arrayPrinting{
+//     public static void main(String[] args){
+//         int[] nums= {23,45,67,89,01};
+//         for(int i:nums){
+//             System.out.println(i);
+//         }
+//     }
+// }
