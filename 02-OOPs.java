223ab0450a1ada8b649d09f9960c23dcc51bbc44
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-29 19:10:43 +0530

    Updated Commit

--- 02-OOPs.java
+++ 02-OOPs.java
@@ -2,7 +2,8 @@
 //Types of constrcutot: parametric and non-Parametric.
 // there is no limit to create constructor in a class.
 
-// what is default constructor? 
+// what is default constructor?  It is a constructor which is provided by the compiler if we don't create any constructor in the class.
+
 import java.util.Scanner;
     public static void main(String[] args){
 
