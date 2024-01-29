27b7be50733c48905ad63eb30916b6260f97fd98
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-29 19:09:20 +0530

    Updated Commit

---
+++ 02-OOPs.java
@@ -0,0 +1,7 @@
+//Constructor: It is a special type of method which is used to initialize the object.
+//Types of constrcutot: parametric and non-Parametric.
+
+import java.util.Scanner;
+    public static void main(String[] args){
+        
+    }
