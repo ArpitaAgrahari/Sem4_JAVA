b8dc2a6e7f4222bc1751c848bc5ae888b69c35cc
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-29 23:02:07 +0530

    Updated Commit

--- Self_Practice/12-Java_Methods.java
+++ Self_Practice/12-Java_Methods.java
@@ -8,9 +8,15 @@ You can pass data, known as parameters, into a method.
 
 Methods are used to perform certain actions, and they are also known as functions.
  */
-import java.util.Scanner;
-class methodLearn{
-    public static void main(String[] args){
-        
-    }
-}
+// import java.util.Scanner;
+// public class hey{    //metodLearn i name of the method
+//      static void methodLearn(){
+//         System.out.println("I just got nervous for a while");
+//     }
+//     public static void main(String[] args){
+//         methodLearn();
+//     }
+// }
+
+
+
