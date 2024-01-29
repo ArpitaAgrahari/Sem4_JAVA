a2c034abd407acc52c43925fd422b1350df08453
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-29 19:10:01 +0530

    Updated Commit

--- 02-OOPs.java
+++ 02-OOPs.java
@@ -1,7 +1,9 @@
 //Constructor: It is a special type of method which is used to initialize the object.
 //Types of constrcutot: parametric and non-Parametric.
+// there is no limit to create constructor in a class.
 
+// what is default constructor? 
 import java.util.Scanner;
     public static void main(String[] args){
-        
+
     }
