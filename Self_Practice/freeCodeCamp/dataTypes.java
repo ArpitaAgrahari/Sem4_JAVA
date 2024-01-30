1079a81af3c645647434c24e384cf82b78a9131c
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-30 22:17:49 +0530

    Updated Commit

--- Self_Practice/freeCodeCamp/dataTypes.java
+++ Self_Practice/freeCodeCamp/dataTypes.java
@@ -1,5 +1,16 @@
 package Self_Practice.freeCodeCamp;
 
-public class dataTypes {
-    
+
+
+//there are two types of data types that are primitive and non-primitive/reference type.
+
+//primitive types: stores  integer values
+//non-primitve/refrence types:  stores the refrnce of a variable or dynamic object is being stored.
+
+
+
+class dataTypes {
+    public static void main(String[] args){
+
+    }
 }
