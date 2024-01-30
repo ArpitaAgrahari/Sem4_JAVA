3348bd124a6e01fd50b83b9cea6e7388208ad140
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-30 22:22:51 +0530

    Updated Commit

--- Self_Practice/freeCodeCamp/dataTypes.java
+++ Self_Practice/freeCodeCamp/dataTypes.java
@@ -12,5 +12,23 @@ package Self_Practice.freeCodeCamp;
 class dataTypes {
     public static void main(String[] args){
 
+        //integer types
+        byte aSingleByte = 100;
+        short aSmallNumber = 20000;
+        int anInteger = 2147483647;
+        long aLargeNumber = 9223372036854775807L; // Updated value
+
+        //decimal types;
+        double aDiuble = 1.79769313;
+        float aFloat = 3.4028F;
+
+        //boolean
+        boolean isWeekand = false;
+        boolean isWorkday = true;
+
+        //character;
+        char copyRightsysmbol = '\u00A9';
+
+        System.out.println("This is copyright symbol: "+copyRightsysmbol);
     }
 }
