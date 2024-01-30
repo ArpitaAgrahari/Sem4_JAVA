e398e03943e83537f9fdbaeebe680f64359a82fd
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-30 22:08:37 +0530

    Updated Commit

--- Self_Practice/freeCodeCamp/Variables.java
+++ Self_Practice/freeCodeCamp/Variables.java
@@ -8,6 +8,8 @@ class Variables {
         // age = 24;
 
         int age=27;
+
+        age = 28;   //we can overwrite the value but wecannot define it more than one time or another time with same variable name
         System.out.println("Given age is:" +age);
     }
 }
