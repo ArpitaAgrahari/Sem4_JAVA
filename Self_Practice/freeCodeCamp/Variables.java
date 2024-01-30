17feb7092debf0c15bc417bbfc4e73e4186bcb7e
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-30 22:13:46 +0530

    Updated Commit

--- Self_Practice/freeCodeCamp/Variables.java
+++ Self_Practice/freeCodeCamp/Variables.java
@@ -1,6 +1,6 @@
 package Self_Practice.freeCodeCamp;
 
-
+/* 
 import java.util.Scanner;
 class Variables {
     public static void main(String[] args){
@@ -9,7 +9,28 @@ class Variables {
 
         int age=27;
 
-        age = 28;   //we can overwrite the value but wecannot define it more than one time or another time with same variable name
+        // age = 28; 
+          //we can overwrite the value but wecannot define it more than one time or another time with same variable name
         System.out.println("Given age is:" +age);
     }
 }
+
+*/
+
+
+
+/*
+ 
+
+import java.util.Scanner;
+class Variables{
+    static int age;
+    public static void main(String[] args){
+        System.out.println("The given age is : "+age);
+    }
+}
+
+//This will run beacuse when we decalre a variable in class level,the java compiler always defines a value for this as a default. for numbers it is 0 always.
+
+
+*/
