e55b55f14803adff7d71062b1e0c7a90e1d996b9
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-30 22:06:33 +0530

    Updated Commit

--- Self_Practice/freeCodeCamp/Variables.java
+++ Self_Practice/freeCodeCamp/Variables.java
@@ -1,5 +1,11 @@
 package Self_Practice.freeCodeCamp;
 
-public class Variables {
-    
+
+import java.util.Scanner;
+class Variables {
+    public static void main(String[] args){
+         int age;
+        age = 24;
+        System.out.println("Given age is:" +age);
+    }
 }
