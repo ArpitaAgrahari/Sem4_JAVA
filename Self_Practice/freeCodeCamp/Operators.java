0c0eaa49679667c4aa0b8e550afbb8cc5e68ef94
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-30 22:37:14 +0530

    Updated Commit

--- Self_Practice/freeCodeCamp/Operators.java
+++ Self_Practice/freeCodeCamp/Operators.java
@@ -1,5 +1,23 @@
 package Self_Practice.freeCodeCamp;
 
-public class Operators {
-    
+class Operators {
+    public static void main(String[] args){
+        int num1 = 12;
+        int num2 = 6;
+
+        //addition
+        System.out.println(num1 + num2);
+
+        //Subtarction
+        System.out.println(num1 - num2);
+
+        //multiplication
+        System.out.println(num1 * num2);
+
+        //division
+        System.out.println(num1/num2);
+
+        //reminder
+        System.out.println(num1%num2);
+    }
 }
