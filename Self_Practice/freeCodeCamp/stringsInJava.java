e73a41a035b4cf7bb70af6010f438a2d448d6a53
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-31 09:26:25 +0530

    Updated Commit

---
+++ Self_Practice/freeCodeCamp/stringsInJava.java
@@ -0,0 +1,21 @@
+package Self_Practice.freeCodeCamp;
+
+
+/* 
+import java.util.Scanner;
+class stringsInJava {
+    public static void main(String[] args){
+        char percentSign = '%';
+        System.out.println(percentSign);
+    }
+}
+
+*/
+
+import java.util.Scanner;
+class stringsInJava{
+    public static void main(String[] args){
+        String name = "hey!! i am arpita";
+        System.out.println(name);
+    }
+}
