41f9dcb5e2c6bc6e86e971ea520938c7c7fb6881
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-31 09:08:46 +0530

    Updated Commit

--- Self_Practice/freeCodeCamp/Operators.java
+++ Self_Practice/freeCodeCamp/Operators.java
@@ -46,7 +46,8 @@ class Operators{
 
 */
 
-
+/* 
+//comparison operators
 import java.util.Scanner;
 class Operators{
     public static void main(String[] args){
@@ -62,5 +63,28 @@ class Operators{
         System.out.println(num1 >= num2);
         //is less than equal to 
         System.out.println(num1 <= num2);
+    }
+}
+
+*/
+
+//logical operators
+import java.util.Scanner;
+class Operators{
+    public static void main(String[] args){
+        /* 
+        int age  =25;
+        //age>=18
+        //age<=40
+        System.out.println(age >= 18 && age<=40);
+        */
+        /* 
+        boolean isStudent = false;
+        boolean isLibararyMember = true;
+        System.out.println(isStudent || isLibararyMember);
+        */
+
+        boolean isStudent =false;
+        System.out.println(!isStudent);
     }
 }
