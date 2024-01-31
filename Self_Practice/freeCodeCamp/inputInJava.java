7921b29574285dbd1a9b099b9a37ca06a0573dde
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-31 10:35:31 +0530

    Updated Commit

--- Self_Practice/freeCodeCamp/inputInJava.java
+++ Self_Practice/freeCodeCamp/inputInJava.java
@@ -1,5 +1,13 @@
 package Self_Practice.freeCodeCamp;
 
-public class inputInJava {
-    
+import java.util.Scanner;
+class inputInJava {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is ur name?");
+        String name = scanner.nextLine();
+        System.out.println("Your name is: "+name);
+
+        scanner.close();
+    }  
 }
