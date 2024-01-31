58834a8fa6542c7826d4b1dc2d62bbf3492b4716
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-31 20:13:00 +0530

    Updated Commit

--- Self_Practice/freeCodeCamp/forLoop.java
+++ Self_Practice/freeCodeCamp/forLoop.java
@@ -1,5 +1,25 @@
 package Self_Practice.freeCodeCamp;
 
-public class forLoop {
-    
+
+
+/* 
+import java.util.Scanner;
+class forLoop {
+    public static void main(String[] args){
+        for(int i = 1;i<=10;i++){
+            System.out.println("Hello there!!");
+        }
+    }
+}
+*/
+
+
+import java.util.Scanner;
+class forLoop{
+    public static void main(String[] args){
+        int number[] = {1,2,3,4,5,6,7,8,9,10};
+        for(int i=1;i<number.length;i++){
+            System.out.println(number[i]);
+        }
+    }
 }
