873a80cf43aa3d667b2db6ce8ca317dd99626da9
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-31 18:46:05 +0530

    Updated Commit

--- Self_Practice/freeCodeCamp/arrayInJava.java
+++ Self_Practice/freeCodeCamp/arrayInJava.java
@@ -1,5 +1,19 @@
 package Self_Practice.freeCodeCamp;
 
-public class arrayInJava {
-    
+import java.util.Arrays;
+class arrayInJava {
+    public static void main(String[] args){
+        char vowels[] = new char[5];
+        //a e i o u
+        vowels[0] = 'a';
+        vowels[1] = 'e';
+        vowels[2] = 'i';
+        vowels[3] = 'o';
+        vowels[4] = 'u';
+
+
+        //System.out.println(vowels[2]);
+        System.out.println(Arrays.toString(vowels));
+        
+    }
 }
