52fec2898dbd45af60d14a653673650cb1937404
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-31 19:11:23 +0530

    Updated Commit

--- Self_Practice/freeCodeCamp/arrayInJava.java
+++ Self_Practice/freeCodeCamp/arrayInJava.java
@@ -3,6 +3,7 @@ package Self_Practice.freeCodeCamp;
 import java.util.Arrays;
 class arrayInJava {
     public static void main(String[] args){
+        /*
         char vowels[] = new char[5];
         //a e i o u
         vowels[0] = 'a';
@@ -10,10 +11,27 @@ class arrayInJava {
         vowels[2] = 'i';
         vowels[3] = 'o';
         vowels[4] = 'u';
+            */
 
+        char vowels[] = {'e','u','i','a','o'};
 
-        //System.out.println(vowels[2]);
-        System.out.println(Arrays.toString(vowels));
+            //System.out.println(vowels[2]);
         
+            //inbuilt array printing 
+        //System.out.println(Arrays.toString(vowels));
+
+            //to get length 
+        //System.out.println(vowels.length);
+
+            //to sort
+        //Arrays.sort(vowels);
+        //System.out.println(Arrays.toString(vowels));
+        
+
+            //if we want to perform sorting in a finite range in array
+        int start = 1;
+        int end = 4; 
+        Arrays.sort(vowels,start,end);
+        System.out.println(Arrays.toString(vowels));
     }
 }
