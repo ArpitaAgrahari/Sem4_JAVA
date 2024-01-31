b56aa04ced39e11f1897e5d0f726049d41473cba
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-31 13:01:38 +0530

    Updated Commit

--- Self_Practice/freeCodeCamp/switchCase.java
+++ Self_Practice/freeCodeCamp/switchCase.java
@@ -3,6 +3,32 @@ package Self_Practice.freeCodeCamp;
 import java.util.Scanner;
 class switchCase {
    public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
 
+        System.out.println("enter the first number:");
+        double num1 = scanner.nextDouble();
+        scanner.nextLine();
+
+        System.out.println("Enter the second number");
+        double num2 = scanner.nextDouble();
+        scanner.nextLine();
+
+        System.out.println("What operators do u want to perform ? ");
+        String operations = scanner.nextLine();
+
+        switch(operations){
+            case "Addition":
+                System.out.printf("%f + %f = %f",num1,num2,num1+num2);
+                break;
+            case "Subtraction":
+                System.out.printf("%f - %f = %f",num1,num2,num1-num2);
+                break;
+            case "Multiplication":
+                System.out.printf("%f * %f = %f",num1,num2,num1*num2);
+                break;
+            case "Divison":
+                System.out.printf("%f / %f = %f",num1,num2,num1/num2);
+                break;
+        }
    } 
 }
