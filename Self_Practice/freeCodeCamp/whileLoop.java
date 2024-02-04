d76806baa8ae7b6745b89722de473578f14c6cd1
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-02-04 19:41:29 +0530

    Updated Commit

--- Self_Practice/freeCodeCamp/whileLoop.java
+++ Self_Practice/freeCodeCamp/whileLoop.java
@@ -3,7 +3,8 @@ package Self_Practice.freeCodeCamp;
 class whileLoop {
     public static void main(String[] args){
         int i = 1;
-        while(i<=10){
+        int num = 5;
+        while(i<=num){
             System.out.println(i);
             i++;
         }
