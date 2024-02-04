f8dbde6e391fe124f34e0a2286bc241beff2fed2
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-02-04 20:20:37 +0530

    Updated Commit

--- Self_Practice/freeCodeCamp/ArrayListInJava.java
+++ Self_Practice/freeCodeCamp/ArrayListInJava.java
@@ -8,11 +8,11 @@ class ArrayListInJava {
 
         //int double float boolean
         //Integer Double Float Boolean
-        number.add(1);  //0
-        number.add(2);  //1
-        number.add(3);  //2
-        number.add(4);  //3
-        number.add(5);  //4
+        number.add(4);  //0
+        number.add(1);  //1
+        number.add(5);  //2
+        number.add(3);  //3
+        number.add(2);  //4
 
         System.out.println(number.toString());
         //to fetch a particular element we can use get() command
@@ -29,7 +29,7 @@ class ArrayListInJava {
         //to set values of element in array
         number.set(2, Integer.valueOf(30));
         System.out.println(number.toString());
-        
+
         //to clear the array 
         number.clear();
 
