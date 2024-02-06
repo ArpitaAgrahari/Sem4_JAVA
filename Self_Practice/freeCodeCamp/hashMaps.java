281a5218879ee989a14a1fcdaa3362a2c6ed2f0d
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-02-06 19:12:20 +0530

    Updated Commit

--- Self_Practice/freeCodeCamp/hashMaps.java
+++ Self_Practice/freeCodeCamp/hashMaps.java
@@ -1,5 +1,17 @@
 package Self_Practice.freeCodeCamp;
 
+import java.util.Scanner;
+import java.util.HashMap;
+
 class hashMaps {
-    
+    public static void main(String[] args){
+        HashMap<String,Integer> examScore = new HashMap<String, Integer>();
+        examScore.put("Math",75);
+        examScore.put("Sociology",85);
+        examScore.put("English",95);
+        examScore.put("Computer Programming",95);
+
+
+        System.out.println(examScore.toString());
+    }
 }
